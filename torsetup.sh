940d3c5cbc07708b04929f4f658852c675f04fd8
Mo80 <139662868+mo8080@users.noreply.github.com>
2023-07-16 15:19:48 -0500

    torsetup.sh

--- torsetup.sh
+++ torsetup.sh
@@ -1,2 +1,4 @@
 wget https://www.torproject.org/dist/torbrowser/12.5.1/tor-browser-linux64-12.5.1_ALL.tar.xz
 tar -xf tor-browser-linux64-12.5.1_ALL.tar.xz
+cd tor-browser
+./start-tor-browser.desktop
